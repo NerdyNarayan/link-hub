0da773ba05dc751214ce980bf859a0e1f7d66197
NerdyNarayan <nerdynarayan@gmail.com>
2024-07-25 21:25:35 +0530

    Adding valid links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { CgProfile } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 export default function HomePage() {
   return <div className=" container h-full  max-w-3xl">
+    
     <div className="h-20 "/>
     
 <div className="absolute top-0 -z-10 h-full w-full "><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px] "></div></div>
@@ -19,18 +20,29 @@ export default function HomePage() {
       <div className="text-2xl mt-4 font-extrabold text-primary">Apurva Narayan Pradhan</div>
 <div className="mt-2 text-primary/80">Full Stack Web developer </div>
 <div className="flex  mt-4 flex-row gap-4 text-2xl">
-  <FaGithub className="hover:text-primary/80"/>
-<FaTwitter className="hover:text-primary/80"/>
+
+  
+<Link href={"https://x.com/NerdyNarayan"}>
+<FaTwitter className="  hover:text-primary/80"/>
+
+</Link>
+<Link href={"youtube.com"}>
 <FaYoutube className="hover:text-primary/80"/>
+
+</Link>
+
+<Link href={"https://www.linkedin.com/in/apurva-narayan-pradhan/"}>
 <FaLinkedin className="hover:text-primary/80"/>
-<FaYoutube className="hover:text-primary/80"/>
+</Link>
+<Link href={""}>
 <IoIosMail className="hover:text-primary/80"/>
+ </Link>
 
 
 </div>
 <div className="flex mt-4 flex-col gap-4">
 
-<Link  href={""}>
+<Link href={"https://my-portfolio-delta-blue-89.vercel.app"}>
 <Button variant={"outline"} className="relative px-[200px] py-6">
 <div>
 <CgProfile  className="absolute top-3 items-center left-8 text-2xl"/>
@@ -42,7 +54,7 @@ export default function HomePage() {
 </Button>
 
 </Link>
-<Link  href={""}>
+<Link  href={"https://my-portfolio-delta-blue-89.vercel.app/blog"}>
 <Button variant={"outline"} className="w-full relative py-6">
 <div>
 <ImBlog  className="absolute top-3 items-center left-8 text-2xl"/>

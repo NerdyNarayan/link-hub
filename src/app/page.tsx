d5ca33c4e5bd28c4a00c8b5b46b1721fdb3c301b
NerdyNarayan <nerdynarayan@gmail.com>
2024-07-25 08:51:05 +0530

    Making ui changes making it look better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,25 @@ import { IoIosMail } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 export default function HomePage() {
-  return <div className=" container max-w-3xl">
+  return <div className=" container h-full  max-w-3xl">
     <div className="h-20 "/>
     
-<div className="absolute top-0 -z-10 h-full w-full "><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div></div>
+<div className="absolute top-0 -z-10 h-full w-full "><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px] "></div></div>
     <div className=" flex flex-col items-center justify-center ">
-      <Image className="rounded-full    w-[140px] h-[140px]" src={"/Profile.jpg"} alt={""} width={150} height={100}/>
+     <div className="absolute top-20 -z-10 h-[140px] w-[140px] rounded-full  bg-[#fff] opacity-90 blur-[10px]  ">
+   
+
+     </div>
+     <Image className=" rounded-full   w-[140px] h-[140px]" src={"/Profile.jpg"} alt={""} width={150} height={100}/>
       <div className="text-2xl mt-4 font-extrabold text-primary">Apurva Narayan Pradhan</div>
 <div className="mt-2 text-primary/80">Full Stack Web developer </div>
-<div className="flex mt-4 flex-row gap-4 text-2xl">
-  <FaGithub/>
-<FaTwitter/>
-<FaYoutube/>
-<FaLinkedin/>
-<FaYoutube/>
-<IoIosMail/>
+<div className="flex  mt-4 flex-row gap-4 text-2xl">
+  <FaGithub className="hover:text-primary/80"/>
+<FaTwitter className="hover:text-primary/80"/>
+<FaYoutube className="hover:text-primary/80"/>
+<FaLinkedin className="hover:text-primary/80"/>
+<FaYoutube className="hover:text-primary/80"/>
+<IoIosMail className="hover:text-primary/80"/>
 
 
 </div>
